8ad64a3111fa5c24b1665ce2135b6cc7e6ffbb30
tejido18 <uo250821@uniovi.es>
2021-04-20 12:13:01 +0200

    SDI-2021-214-10.2-SW API REST Autenticación.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 let express = require('express');
 let app = express();
 
+let jwt = require('jsonwebtoken');
+app.set('jwt',jwt);
+
 let fs = require('fs');
 let https = require('https');
 
@@ -16,12 +19,49 @@ var fileUpload = require('express-fileupload');
 app.use(fileUpload());
 var mongo = require('mongodb');
 var swig = require('swig');
+
+var gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var gestorBD = require("./modules/gestorBD.js");
-gestorBD.init(app,mongo);
+// routerUsuarioToken
+let routerUsuarioToken = express.Router();
+routerUsuarioToken.use(function(req, res, next) {
+    // obtener el token, vía headers (opcionalmente GET y/o POST).
+    let token = req.headers['token'] || req.body.token || req.query.token;
+    if (token != null) {
+        // verificar el token
+        jwt.verify(token, 'secreto', function(err, infoToken) {
+            if (err || (Date.now()/1000 - infoToken.tiempo) > 240 ){
+                res.status(403); // Forbidden
+                res.json({
+                    acceso : false,
+                    error: 'Token invalido o caducado'
+                });
+                // También podríamos comprobar que intoToken.usuario existe
+                return;
+
+            } else {
+                // dejamos correr la petición
+                res.usuario = infoToken.usuario;
+                next();
+            }
+        });
+
+    } else {
+        res.status(403); // Forbidden
+        res.json({
+            acceso : false,
+            mensaje: 'No hay Token'
+        });
+    }
+});
+
+// Aplicar routerUsuarioToken
+app.use('/api/cancion', routerUsuarioToken);
 
 // routerUsuarioSession
 var routerUsuarioSession = express.Router();
